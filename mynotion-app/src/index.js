0653eefc8102ae506256c505ca32f6b27360a492
melni <melannie.nimocks@duke.edu>
2022-09-23 15:53:46 -0400

    fix integration after notion update

--- mynotion-app/src/index.js
+++ mynotion-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {Client} from "@notionhq/client"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +12,34 @@ root.render(
   </React.StrictMode>
 );
 
+const notion = new Client({ auth: process.env.NOTION_KEY })
 
+const databaseId = process.env.NOTION_DATABASE_ID
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: { 
+          title:[
+            {
+              "text": {
+                "content": text
+              }
+            }
+          ]
+        }
+      },
+    })
+    console.log(response)
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+addItem("Yurts in Big Sur, California")
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
