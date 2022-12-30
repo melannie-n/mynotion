dd230f632ea6cdfeda3f140d2da91e415ca6761f
melni <melannie.nimocks@duke.edu>
2022-12-30 17:01:53 -0500

    add notion client

---
+++ frontend/editNotion.js
@@ -0,0 +1,31 @@
+import { Client } from "@notionhq/client"
+
+const notion = new Client({ auth: "KEY"})
+
+const databaseId = "ID"
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title:[
+            {
+              "text": {
+                "content": text
+              }
+            }
+          ]
+        }
+      },
+    })
+    console.log(response)
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+addItem("data")
+
