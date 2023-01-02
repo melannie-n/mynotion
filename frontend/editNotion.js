c8311aea858e4fbbf12f5b4db35a000de1bd4a17
melni <melannie.nimocks@duke.edu>
2023-01-02 16:40:33 -0500

    successfully return list of all text on a given page

--- frontend/editNotion.js
+++ frontend/editNotion.js
@@ -1,8 +1,44 @@
 import { Client } from "@notionhq/client"
 
-const notion = new Client({ auth: "KEY"})
+
+const notion = new Client({ auth: 'Insert Key'})
 
 const databaseId = "ID"
+
+const pageId = "Insert ID"
+
+async function getPage(){
+  try{
+  const blockId = pageId;
+  const response = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50
+  });
+  var data = response.results;
+  var dataList = {};
+  for (var entry in data){
+    var i = Object.keys(data).indexOf(entry);
+    var dataEntry = data[i].paragraph;
+    dataList[i]=dataEntry;
+  }
+  var textList = {};
+  for (var entry in dataList){
+    if (dataList[entry]){
+      textList[entry]=(dataList[entry]['rich_text']);
+    }
+  }
+  var text = []
+  for (var textVal in textList){
+    if (textList[textVal][0]['plain_text']){
+      var i = Object.keys(textList).indexOf(textVal);
+      text[i]=textList[textVal][0]['plain_text'];
+    }
+  }
+  //console.log(text);
+  } catch (error) {
+    console.error(error.body)
+  }
+}
 
 async function addItem(text) {
   try {
@@ -27,5 +63,4 @@ async function addItem(text) {
   }
 }
 
-addItem("data")
-
+console.log(getPage());
