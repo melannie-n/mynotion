cab56febf4b4e68b53f57c7de22e8de9099c74b4
melni <melannie.nimocks@duke.edu>
2023-01-03 22:25:17 -0500

    working database queries displayed on react frontend

--- notion-app/src/App.tsx
+++ notion-app/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// Copy the payload shape interface from our server
+// We want to copy (rather than import) since we we won't necessarily deploy our
+// front end and back end to the same place
+interface ThingToLearn {
+  label: string;
+  url: string;
+}
 
 function App() {
+  // A state value will store the current state of the array of data which can be updated
+  // by editing your database in Notion and then pressing the fetch button again
+  const [thingsToLearn, setThingsToLearn] = useState<ThingToLearn[]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Data</h1>
+      <button
+        type="button"
+        onClick={() => {
+          fetch("http://localhost:8000/")
+            .then((response) => response.json())
+            .then((payload) => {
+              // Set the React state with the array response
+              setThingsToLearn(payload);
+            });
+        }}
+      >
+        Fetch List
+      </button>
+
+      {/* Map the resulting object array into an ordered HTML list with anchor links */}
+      {/* Using index as key is harmless since we will only ever be replacing the full list */}
+      <ol>
+        {thingsToLearn.map((thing, idx) => {
+          return (
+            <li key={idx}>
+              <a href={thing.url} target="_blank" rel="noopener noreferrer">
+                {thing.label}
+              </a>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
