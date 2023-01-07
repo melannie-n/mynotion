b4f28749dc059110b4e0e1974d826b9c7bb3aee8
melni <melannie.nimocks@duke.edu>
2023-01-07 18:37:56 -0500

    can toggle between different notion pages on frontend

--- notion-app/src/pages/Study.tsx
+++ notion-app/src/pages/Study.tsx
@@ -5,31 +5,38 @@ import {FlashcardArray} from "react-quizlet-flashcard";
 // We want to copy (rather than import) since we we won't necessarily deploy our
 // front end and back end to the same place
 interface ThingToLearn {
-  label: string;
-  url: string;
+  term: string;
+  definition: string;
 }
 
 export function Study() {
   // A state value will store the current state of the array of data which can be updated
   // by editing your database in Notion and then pressing the fetch button again
-  const [thingsToLearn, setThingsToLearn] = useState<ThingToLearn[]>([]);
+  var [thingsToLearn, setThingsToLearn] = useState<ThingToLearn[]>([]);
 
   return (
     <div>
       <div style={{textAlign: "center"}}>
       <button className = "button-17"
-        type="button"
-        onClick={() => {
-          fetch("http://localhost:8000/")
-            .then((response) => response.json())
-            .then((payload) => {
-              // Set the React state with the array response
-              setThingsToLearn(payload);
-            });
-        }}
-      >
-        Fetch List
-      </button>
+                  type="button"
+                  onClick={() => {
+                    fetch("http://localhost:8000/"+"0")
+                      .then((response) => response.json())
+                      .then((payload) => {
+                        // Set the React state with the array response
+                        setThingsToLearn(payload);
+                      });
+                  }}> Fetch List 1 </button>
+      <button className = "button-17"
+                  type="button"
+                  onClick={() => {
+                    fetch("http://localhost:8000/"+"1")
+                      .then((response) => response.json())
+                      .then((payload) => {
+                        // Set the React state with the array response
+                        setThingsToLearn(payload);
+                      });
+                  }}> Fetch List 2 </button>
       </div>
       <div className = "cardBlock">
         <FlashcardArray 
@@ -44,8 +51,8 @@ export function Study() {
         cards = {thingsToLearn.map((thing, idx) => {
           return (
             {id: idx,
-            frontHTML:<p className = "cardText">{thing.label}</p>,
-            backHTML: <p className = "cardText">{thing.url}</p>
+            frontHTML:<p className = "cardText">{thing.term}</p>,
+            backHTML: <p className = "cardText">{thing.definition}</p>
             }
           );
         })}/>
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,19 +8,26 @@ import { getPageProperty } from "@notionhq/client/build/src/api-endpoints";
 // When the data is queried it will come back in a much more complicated shape, so our goal is to
 // simplify it to make it easy to work with on the front end
 interface ThingToLearn {
-  label: string;
-  url: string;
+  term: string;
+  definition: string;
 }
 
 // The dotenv library will read from your .env file into these values on `process.env`
-const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+const notionDatabaseId = process.env.NOTION_DATABASE_ID?.split(" ");
 const notionSecret = process.env.NOTION_SECRET;
 
-// Will provide an error to users who forget to create the .env file
-// with their Notion data in it
-if (!notionDatabaseId || !notionSecret) {
-  throw Error("Must define NOTION_SECRET and NOTION_DATABASE_ID in env");
+//check if database and secret key env variables set
+if (!notionDatabaseId){
+  throw Error("Must define at least one database.");
 }
+
+//ensure all database IDs are valid
+for (var db of notionDatabaseId){
+  if (!db) {
+    throw Error("Must define NOTION_SECRET and NOTION_DATABASE_ID in env");
+  }
+}
+
 
 // Initializing the Notion client with your secret
 const notion = new Client({
@@ -29,56 +36,96 @@ const notion = new Client({
 
 const host = "localhost";
 const port = 8000;
+const db_id1 = notionDatabaseId[0];
+const db_id2 = notionDatabaseId[1];
 
 // Require an async function here to support await with the DB query
 const server = http.createServer(async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+    switch (req.url) {
+      case "/0":
+        // Query the database and wait for the result
+        const query = await notion.databases.query({
+          database_id: db_id1,
+        });
+        // We map over the complex shape of the results and return a nice clean array of
+        // objects in the shape of our `ThingToLearn` interface
+        const list: ThingToLearn[] = query.results.map((row) => {
+          // row represents a row in our database and the name of the column is the
+          // way to reference the data in that column
+          const labelCell = row.properties.term;
+          const urlCell = row.properties.definition;
+  
+          // Depending on the column "type" we selected in Notion there will be different
+          // data available to us (URL vs Date vs text for example) so in order for Typescript
+          // to safely infer we have to check the `type` value.  We had one text and one url column.
+          const isLabel = labelCell.type === "rich_text";
+          const isUrl = urlCell.type === "rich_text";
+  
+          // Verify the types are correct
+          if (isLabel && isUrl) {
+            // Pull the string values of the cells off the column data
+            const term = labelCell.rich_text?.[0].plain_text;
+            const definition = urlCell.rich_text?.[0].plain_text;
+  
+            // Return it in our `ThingToLearn` shape
+            return { term, definition };
+          }
+  
+          // If a row is found that does not match the rules we checked it will still return in the
+          // the expected shape but with a NOT_FOUND label
+          return { term: "NOT_FOUND", definition: "" };
+        });
+  
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify(list));
+        break;
 
-  switch (req.url) {
-    case "/":
-      // Query the database and wait for the result
-      const query = await notion.databases.query({
-        database_id: notionDatabaseId,
-      });
-      // We map over the complex shape of the results and return a nice clean array of
-      // objects in the shape of our `ThingToLearn` interface
-      const list: ThingToLearn[] = query.results.map((row) => {
-        // row represents a row in our database and the name of the column is the
-        // way to reference the data in that column
-        const labelCell = row.properties.label;
-        const urlCell = row.properties.url;
-
-        // Depending on the column "type" we selected in Notion there will be different
-        // data available to us (URL vs Date vs text for example) so in order for Typescript
-        // to safely infer we have to check the `type` value.  We had one text and one url column.
-        const isLabel = labelCell.type === "rich_text";
-        const isUrl = urlCell.type === "url";
-
-        // Verify the types are correct
-        if (isLabel && isUrl) {
-          // Pull the string values of the cells off the column data
-          const label = labelCell.rich_text?.[0].plain_text;
-          const url = urlCell.url ?? "";
-
-          // Return it in our `ThingToLearn` shape
-          return { label, url };
-        }
-
-        // If a row is found that does not match the rules we checked it will still return in the
-        // the expected shape but with a NOT_FOUND label
-        return { label: "NOT_FOUND", url: "" };
-      });
-
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(200);
-      res.end(JSON.stringify(list));
-      break;
-
-    default:
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(404);
-      res.end(JSON.stringify({ error: "Resource not found" }));
-  }
+        case "/1":
+          // Query the database and wait for the result
+          const query1 = await notion.databases.query({
+            database_id: db_id2,
+          });
+          // We map over the complex shape of the results and return a nice clean array of
+          // objects in the shape of our `ThingToLearn` interface
+          const list1: ThingToLearn[] = query1.results.map((row) => {
+            // row represents a row in our database and the name of the column is the
+            // way to reference the data in that column
+            const labelCell = row.properties.term;
+            const urlCell = row.properties.definition;
+    
+            // Depending on the column "type" we selected in Notion there will be different
+            // data available to us (URL vs Date vs text for example) so in order for Typescript
+            // to safely infer we have to check the `type` value.  We had one text and one url column.
+            const isLabel = labelCell.type === "rich_text";
+            const isUrl = urlCell.type === "rich_text";
+    
+            // Verify the types are correct
+            if (isLabel && isUrl) {
+              // Pull the string values of the cells off the column data
+              const term = labelCell.rich_text?.[0].plain_text;
+              const definition = urlCell.rich_text?.[0].plain_text;
+    
+              // Return it in our `ThingToLearn` shape
+              return { term, definition };
+            }
+    
+            // If a row is found that does not match the rules we checked it will still return in the
+            // the expected shape but with a NOT_FOUND label
+            return { term: "NOT_FOUND", definition: "" };
+          });
+    
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.end(JSON.stringify(list1));
+          break;
+  
+      default:
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(404);
+        res.end(JSON.stringify({ error: "Resource not found" }));
+    }
 });
 
 server.listen(port, host, () => {
