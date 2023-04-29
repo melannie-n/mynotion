de010d90f4abe23258deed1e7eb7a22b5f530441
mpn12 <mpn12@duke.edu>
2023-04-29 16:32:57 -0400

    ui updates

--- notion-app/src/App.tsx
+++ notion-app/src/App.tsx
@@ -3,10 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "./NavBar";
 import { Study } from "./pages/Study";
 import {Home} from "./pages/Home"
+import './App.css';
+
+const background = require('./images/16080076_Landscape_with_mountains_at_sunset_1103.jpg');
+const divStyle = {
+  width: '100vw',
+  height: '100vh',
+  backgroundRepeat: 'no-repeat',
+  backgroundImage: `url(${background})`,
+  backgroundSize: 'cover',
+};
 
 function App() {
   return (
-    <>
+    <div className ="app" style = {divStyle}>
+      <>
       <BrowserRouter>
         <NavBar />
         <Routes>
@@ -16,7 +27,8 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+   </>
+   </div>
   );
 }
 
